c1ebdfb86d8ea5d543c25bc7a9086a0f0079e588
Remmy Clay <remmy.lauren@gmail.com>
2017-12-18 16:43:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   def reverse_transfer
     if self.status == "complete"
       sender.balance += amount
-      recevier.balance -= amount
+      receiver.balance -= amount
     self.status = "pending"
     end
   end
