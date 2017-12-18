c150c373f3a1f3cd2e8712ecd3890af06ee919d0
Remmy Clay <remmy.lauren@gmail.com>
2017-12-18 16:49:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
+    if valid? && receiver.balance > self.amount && self.status == "complete"
       sender.balance += amount
       receiver.balance -= amount
     self.status = "reversed"
